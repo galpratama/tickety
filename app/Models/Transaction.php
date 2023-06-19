b6cc05dc301ebc868179d34f106dadaa89d84a97
Galih Pratama <hanamura.iost@gmail.com>
2023-06-19 10:16:35 +0700

    db: transactions

---
+++ app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'code',
+        'event_id',
+        'name',
+        'email',
+        'status',
+        'fee_price',
+        'unique_price',
+        'total_price',
+        'payment_method',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
