6610dde24081c8899babbac5cbd6dc51b158fb37
Galih Pratama <hanamura.iost@gmail.com>
2023-06-19 09:54:47 +0700

    db: events

---
+++ app/Models/Event.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'headline',
+        'description',
+        'type',
+        'start_date',
+        'location',
+        'duration',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+    ];
+}
---
+++ database/migrations/2023_06_19_024405_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('headline')->nullable();
+            $table->text('description')->nullable();
+            $table->string('type')->default('offline'); // online, offline
+            $table->date('start_date')->nullable();
+            $table->string('location')->nullable();
+            $table->number('duration')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
